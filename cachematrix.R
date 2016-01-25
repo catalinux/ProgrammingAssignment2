f021e2ec5173dd211dae0ff76fa2f8905be839b3
catalin <catalin@run.ro>
2016-01-25 09:41:30 +0200

    working examples & fixx return of matrix data

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,14 @@
 ## functions do
 
 
-##usage of these function s
+##usage of these functions
 ##Example:
 ##B = matrix( c(2, 1, 5, 7), nrow=2, ncol=2)
-##a<-makeCacheMatrix(B)
+##smartMatrix<-makeCacheMatrix(A)
 ##calculates inverse
-##result<-cacheSolve(a)
-##does not calculate, it uses cached data
-##result<-cacheSolve(a)
+##cacheSolve(smartMatrix)
+##
+## Down at the bottom file we have a worinking code that uses those two functions.
 
 
 
@@ -17,6 +17,11 @@
 ## setting and retrieving the cahed result of solve method (inverse)
 
 makeCacheMatrix <- function(x = matrix()) {
+  if(!is.matrix(as.matrix(x))){
+         stop("1st argument should be a matrix")
+  }
+  
+  
   inv <- NULL
   set <- function(y) {
     x <<- y
@@ -32,20 +37,38 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Returns the cache result of `solve` function if there is one set on the smart matrix "object"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m<-x$getinverse
-  if(!is.null(m)) {
+  mInverse<-x$getinverse()
+  if(!is.null(mInverse)) {
     message("getting cached data")
-    return(m)
+    return(mInverse)
   }
   data <- x$get()
-  m <- solve(data)
-  x$setinverse(m)
-  m
+  mInverse <- solve(data)
+  x$setinverse(mInverse)
+  mInverse
 }
 
 
+#example
 
+
+print("Fisrt matrix")
+A <- matrix( c(1, 1, 5, 7), nrow=2, ncol=2)
+smartMatrix<-makeCacheMatrix(A)
+##calculates inverse
+cacheSolve(smartMatrix)
+##does not calculate, it uses cached data
+cacheSolve(smartMatrix)
+
+
+print("Second matrix")
+C <- matrix( c(3, 1, 5, 7), nrow=2, ncol=2)
+smartMatrix<-makeCacheMatrix(C)
+##calculates inverse
+cacheSolve(smartMatrix)
+##does not calculate, it uses cached data
+cacheSolve(smartMatrix)
